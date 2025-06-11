7ccd26ab4b5950b8ec6d295a8c3b56bc22f4bb55
sravan8888 <mutyalasravan8@gmail.com>
2025-06-11 16:06:55 +0000

    there new files build.sh,deploy.sh and index.html files are added

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Building the application..."
+mkdir -p output
+cp index.html output/
+echo "Build complete."
+
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Deploying the application..."
+cp -r output/* /var/www/html/
+echo "Deployment complete."
+
